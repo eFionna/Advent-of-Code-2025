cfc305a3d8c4f80720e86c93b44cbbd08fc62be3
eFionna <56249078+eFionna@users.noreply.github.com>
2025-12-01 12:15:41 +0100

    Day One: Part One

--- Day1/DayOne.cs
+++ Day1/DayOne.cs
@@ -5,12 +5,46 @@ namespace AdventOfCode2025.Day1;
 internal class DayOne : AdventDay
 {
     internal override int Day => 1;
-    public DayOne() : base(){}
 
+    private int dial = 50;
+
+    private readonly List<string> instructions;
+
+    public DayOne() : base()
+    {
+
+        instructions = Input.Split(Environment.NewLine, StringSplitOptions.TrimEntries).ToList();
+    }
+
+
+    private void RotateDial(string instruction)
+    {
+        bool isLeft = char.Equals(instruction[0], 'L');
+        int clicks = int.Parse(instruction[1..]);
+        Console.WriteLine("Clicks: " + instruction[1..]);
+        dial = isLeft ? dial - clicks : dial + clicks;
+
+        var rotationCorrection = Math.Abs(dial) / 100;
+        if (dial < 0)
+        {
+            dial += 100 * rotationCorrection;
+        }
+        else if (dial > 99)
+        {
+            dial -= 100 * rotationCorrection;
+        }
+    }
 
     internal override int PartOne()
     {
-        return base.PartOne();
+        int isAtZero = 0;
+        foreach (string instruction in instructions) 
+        {
+            RotateDial(instruction);
+            if (dial == 0)
+                isAtZero++;
+        }
+        return isAtZero;
     }
 
     internal override int PartTwo()
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2025.Day1;
 
-
+DayOne dayOne = new();
+Console.WriteLine(dayOne.PartOne());
 
 Console.ReadLine();
