5a06685094f91b8195d1ff66a209573e30a0b17f
eFionna <56249078+eFionna@users.noreply.github.com>
2025-12-05 16:59:53 +0100

    Day Five Part One

---
+++ Day5/DayFive.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode2025.Utility;
+
+namespace AdventOfCode2025.Day5;
+
+internal class DayFive : AdventDay
+{
+    internal override int Day => 5;
+
+    private readonly List<Tuple<long, long>> ingredientIDRanges;
+    private readonly List<long> ingredientIDs;
+
+    internal DayFive() : base()
+    {
+        ingredientIDRanges = [];
+        ingredientIDs = [];
+        string[] database = Input.Split([Environment.NewLine + Environment.NewLine], StringSplitOptions.RemoveEmptyEntries);
+
+        string[] tempRanges = database[0].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string range in tempRanges)
+        {
+            var split = range.Split('-', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            ingredientIDRanges.Add(new(long.Parse(split[0]), long.Parse(split[1])));
+        }
+        string[] tempIds = database[1].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string id in tempIds)
+        {
+            ingredientIDs.Add(long.Parse(id));
+        }
+    }
+
+    internal override string PartOne()
+    {
+        long freshIngredientIDs = 0;
+        foreach (long id in ingredientIDs)
+        {
+            foreach (Tuple<long, long> range in ingredientIDRanges)
+            {
+                if (id >= range.Item1 && id <= range.Item2)
+                {
+                    freshIngredientIDs++;
+                    break;
+                }
+            }
+        }
+        return freshIngredientIDs.ToString();
+    }
+
+
+    internal override string PartTwo()
+    {
+        return base.PartTwo();
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using AdventOfCode2025.Day2;
 using AdventOfCode2025.Day3;
 using AdventOfCode2025.Day4;
+using AdventOfCode2025.Day5;
 using AdventOfCode2025.Utility;
 
 Dictionary<int, AdventDay> days = new()
@@ -9,11 +10,12 @@ Dictionary<int, AdventDay> days = new()
     { 1, new DayOne() },
     { 2, new DayTwo() },
     { 3, new DayThree() },
-    { 4, new DayFrour() }
+    { 4, new DayFrour() },
+    { 5, new DayFive() },
 };
 
-Console.WriteLine(days[4].PartOne());
-Console.WriteLine(days[4].PartTwo());
+Console.WriteLine(days[5].PartOne());
+Console.WriteLine(days[5].PartTwo());
 
 
 
