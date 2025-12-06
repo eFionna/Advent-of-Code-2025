7a817aa8b0862b18d935b0a3a4f2acbb49bfa0b3
eFionna <56249078+eFionna@users.noreply.github.com>
2025-12-06 12:58:49 +0100

    Day Six Part One

---
+++ Day6/DaySix.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode2025.Utility;
+
+namespace AdventOfCode2025.Day6;
+
+internal class DaySix : AdventDay
+{
+    internal override int Day => 6;
+    private readonly Dictionary<int, List<string>> problems = [];
+    internal DaySix() : base()
+    {
+        string[] split = Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        for (int y = 0; y < split.Length; y++)
+        {
+            string line = split[y];
+            string[] m = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            for (int x = 0; x < m.Length; x++)
+            {
+                string val = m[x];
+                if (problems.TryGetValue(x, out var li))
+                {
+                    li.Add(val);
+                }
+                else
+                {
+                    problems[x] = [val];
+                }
+            }
+        }
+    }
+
+    internal override string PartOne()
+    {
+        long final = 0;
+        foreach (List<string> li in problems.Values)
+        {
+            string last = li.Last();
+            long num = long.Parse(li.First()!);
+
+            Func<long, long, long> operation = last == "*" ? (a, b) => a * b : (a, b) => a + b;
+
+            foreach (string val in li.SkipLast(1).Skip(1))
+            {
+                long v = long.Parse(val);
+                num = operation(num, v);
+            }
+            final += num;
+        }
+
+
+        return final.ToString();
+    }
+
+    internal override string PartTwo() 
+    {
+        return PartTwo();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,19 +3,21 @@ using AdventOfCode2025.Day2;
 using AdventOfCode2025.Day3;
 using AdventOfCode2025.Day4;
 using AdventOfCode2025.Day5;
+using AdventOfCode2025.Day6;
 using AdventOfCode2025.Utility;
 
 Dictionary<int, AdventDay> days = new()
 {
-    { 1, new DayOne() },
-    { 2, new DayTwo() },
-    { 3, new DayThree() },
-    { 4, new DayFrour() },
-    { 5, new DayFive() },
+    //{ 1, new DayOne() },
+    //{ 2, new DayTwo() },
+    //{ 3, new DayThree() },
+    //{ 4, new DayFrour() },
+    //{ 5, new DayFive() },
+    { 6, new DaySix() },
 };
 
-Console.WriteLine(days[5].PartOne());
-Console.WriteLine(days[5].PartTwo());
+Console.WriteLine(days[6].PartOne());
+Console.WriteLine(days[6].PartTwo());
 
 
 
