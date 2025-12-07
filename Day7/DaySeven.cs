39c391cdeea2d1c8ea6550dba36fd9719a53e9c8
eFionna <56249078+eFionna@users.noreply.github.com>
2025-12-07 12:55:39 +0100

    Day Seven Part One

---
+++ Day7/DaySeven.cs
@@ -0,0 +1,118 @@
+﻿using AdventOfCode2025.Utility;
+namespace AdventOfCode2025.Day7;
+
+internal enum MoveDir
+{
+    Down,
+    Left,
+    Right
+}
+
+internal class DaySeven : AdventDay
+{
+    internal override int Day => 7;
+
+    private readonly char[,] tachyonManifold;
+    private readonly Tuple<int, int> startPos;
+    private readonly int endY;
+
+    int pXLen;
+    int pYLen;
+
+    internal DaySeven() : base()
+    {
+        var split = Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        int xLen = split[0].Length;
+        int yLen = split.Length;
+        endY = yLen - 1;
+        pXLen = xLen;
+        pYLen = yLen;
+
+
+        tachyonManifold = new char[xLen, yLen];
+
+        for (int y = 0; y < yLen; y++)
+        {
+            var line = split[y];
+            for (int x = 0; x < xLen; x++)
+            {
+                char c = line[x];
+                tachyonManifold[x, y] = c;
+                if (c == 'S')
+                {
+                    startPos = Tuple.Create(x, y);
+                }
+            }
+        }
+        //WE MUST PLEASE THE IDE
+        if (startPos == null)
+        {
+            startPos = Tuple.Create(-1, -1);
+            Console.WriteLine("BAD FUCKING BAD");
+        }
+    }
+
+
+    internal char GetCharAtXY(int x, int y)
+    {
+        return tachyonManifold[x, y];
+    }
+
+    internal override string PartOne()
+    {
+        int curY = startPos.Item2;
+        HashSet<int> allX = [startPos.Item1];
+        int splits = 0;
+        do
+        {
+            curY++;
+            List<int> newX = [];
+            List<int> toRemove = [];
+            foreach (int x in allX)
+            {
+                if (GetCharAtXY(x, curY) != '.')
+                {
+                    splits++;
+                    toRemove.Add(x);
+                    newX.Add(x + 1);
+                    newX.Add(x - 1);
+                }
+            }
+            foreach (int x in toRemove)
+            {
+                allX.Remove(x);
+            }
+            foreach (int x in newX)
+            {
+                allX.Add(x);
+    
+            }
+
+            //for (int y = 0; y < pYLen; y++)
+            //{
+            //    for (int x = 0; x < pXLen; x++)
+            //    {
+            //        if (allX.Contains(x) && y == curY)
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Green;
+            //        }
+
+            //        Console.Write(tachyonManifold[x, y]);
+
+            //        Console.ForegroundColor = ConsoleColor.White;
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("##########################");
+            //Console.WriteLine();
+        }
+        while (curY < endY);
+
+
+
+        return splits.ToString();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using AdventOfCode2025.Day3;
 using AdventOfCode2025.Day4;
 using AdventOfCode2025.Day5;
 using AdventOfCode2025.Day6;
+using AdventOfCode2025.Day7;
 using AdventOfCode2025.Utility;
 
 Dictionary<int, AdventDay> days = new()
@@ -13,12 +14,14 @@ Dictionary<int, AdventDay> days = new()
     //{ 3, new DayThree() },
     //{ 4, new DayFrour() },
     //{ 5, new DayFive() },
-    { 6, new DaySix() },
+    //{ 6, new DaySix() },
+    { 7, new DaySeven() },
+
 };
 
-Console.WriteLine(days[6].PartOne());
+Console.WriteLine(days[7].PartOne());
 Console.WriteLine();
-Console.WriteLine(days[6].PartTwo());
+Console.WriteLine(days[7].PartTwo());
 
 
 
