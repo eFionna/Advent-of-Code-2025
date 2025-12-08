0aca89a3d906d5d8ef02e2ab03b67fd644089dce
eFionna <56249078+eFionna@users.noreply.github.com>
2025-12-08 19:14:46 +0100

    Day Eight Part Two

--- Day8/DayEight.cs
+++ Day8/DayEight.cs
@@ -6,7 +6,7 @@ internal class DayEight : AdventDay
 {
     internal override int Day => 8;
 
-    private readonly List<Tuple<int, int, int>> points3d = [];
+    private readonly List<Tuple<double, double, double>> points3d = [];
     private readonly int boxCount;
     internal DayEight() : base()
     {
@@ -16,12 +16,11 @@ internal class DayEight : AdventDay
         foreach (string line in lines)
         {
             string[] points = line.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-            points3d.Add(Tuple.Create(int.Parse(points[0]), int.Parse(points[1]), int.Parse(points[2])));
+            points3d.Add(Tuple.Create(double.Parse(points[0]), double.Parse(points[1]), double.Parse(points[2])));
         }
     }
 
-
-    internal override string PartOne()
+    private List<Edge> GetSortedEdges()
     {
         List<Edge> edges = [];
 
@@ -31,8 +30,8 @@ internal class DayEight : AdventDay
             for (int j = i + 1; j < points3d.Count; j++)
             {
 
-                Tuple<int, int, int> pointA = points3d[i];
-                Tuple<int, int, int> pointB = points3d[j];
+                Tuple<double, double, double> pointA = points3d[i];
+                Tuple<double, double, double> pointB = points3d[j];
 
                 double dx = pointA.Item1 - pointB.Item1;
                 double dy = pointA.Item2 - pointB.Item2;
@@ -45,6 +44,12 @@ internal class DayEight : AdventDay
         }
 
         var sorted = edges.OrderBy(p => p.Dist).ToList();
+        return sorted;
+    }
+
+    internal override string PartOne()
+    {
+        List<Edge> sorted = GetSortedEdges();
         var dsu = new DisjointUnionSets(boxCount);
 
         int limit = 1000; // The test input requiers the fucking limit to be 10 annoying
@@ -76,7 +81,42 @@ internal class DayEight : AdventDay
         return size.ToString();
     }
 
+  
 
+    internal override string PartTwo()
+    {
+        List<Edge> sorted = GetSortedEdges();
+        Edge lastEdge = null;
+        var dsu = new DisjointUnionSets(boxCount);
+        int components = boxCount;
+
+        foreach (Edge edge in sorted)
+        {
+            int rootA = dsu.Find(edge.A);
+            int rootB = dsu.Find(edge.B);
+
+            if (rootA != rootB)
+            {
+                dsu.Union(edge.A, edge.B);
+                components--;
+                lastEdge = edge;
+
+                if (components == 1) break;
+            }
+        }
+
+
+        if (lastEdge == null) 
+        {
+            throw new NullReferenceException("BAD");
+        }
+
+        var a = points3d[lastEdge.A];
+        var b = points3d[lastEdge.B];
+
+        double final =  a.Item1 * b.Item1;
+        return final.ToString();
+    }
 
     class Edge(int a, int b, double dist)
     {
